d28cdb32bb08aed1627368921a0f5770ea11a588
Mike Smullin <mike@mikesmullin.com>
2023-01-02 01:26:54 -0700

    more smart pointer support

--- src/lib/Vulkan.cpp
+++ src/lib/Vulkan.cpp
@@ -4,6 +4,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
+#include <memory>
 
 #include "Logger.hpp"
 
@@ -14,19 +15,29 @@ const unsigned int ENGINE_MAJOR = 1;
 const unsigned int ENGINE_MINOR = 1;
 const unsigned int ENGINE_REVISION = 1;
 
-VkApplicationInfo Vulkan::DescribeApplication(
+std::unique_ptr<VkApplicationInfo> Vulkan::DescribeApplication(
     const char* name,
     const unsigned int major,
     const unsigned int minor,
     const unsigned int revision) {
-  VkApplicationInfo appInfo{};
-  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-  appInfo.pApplicationName = name;
-  appInfo.applicationVersion = VK_MAKE_VERSION(major, minor, revision);
-  appInfo.pEngineName = ENGINE_NAME;
-  appInfo.engineVersion = VK_MAKE_VERSION(ENGINE_MAJOR, ENGINE_MINOR, ENGINE_REVISION);
-  appInfo.apiVersion = VK_API_VERSION_1_3;
+  auto appInfo = std::make_unique<VkApplicationInfo>();
+  appInfo->sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo->pApplicationName = name;
+  appInfo->applicationVersion = VK_MAKE_VERSION(major, minor, revision);
+  appInfo->pEngineName = ENGINE_NAME;
+  appInfo->engineVersion = VK_MAKE_VERSION(ENGINE_MAJOR, ENGINE_MINOR, ENGINE_REVISION);
+  appInfo->apiVersion = VK_API_VERSION_1_3;
   return appInfo;
+}
+
+void Vulkan::CreateInstance(std::unique_ptr<VkApplicationInfo> appInfo) {
+  auto createInfo = std::make_unique<VkInstanceCreateInfo>();
+  createInfo->sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo->pApplicationInfo = appInfo.get();
+
+  // TODO: finish
+  // https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Instance
+  // https://wiki.libsdl.org/SDL2/SDL_Vulkan_GetInstanceExtensions
 }
 
 }  // namespace mks
