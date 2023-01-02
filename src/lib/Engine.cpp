aa814ad2bdbcaaba090d46eb8242a9d2ea8690a1
Mike Smullin <mike@mikesmullin.com>
2023-01-02 01:12:18 -0700

    correct example of smart pointer use

--- src/lib/Engine.cpp
+++ src/lib/Engine.cpp
@@ -1,9 +1,15 @@
 #include "Engine.hpp"
 
+#include <memory>
+
+#include "Logger.hpp"
+#include "Vulkan.hpp"
+
 namespace mks {
 
-Engine Engine::Init() {
-  Engine e{};
+std::unique_ptr<Engine> Engine::Init() {
+  auto e = std::make_unique<Engine>();
+  e->Run();
   auto appInfo = mks::Vulkan::DescribeApplication("Game", 1, 0, 0);
   return e;
 }
@@ -16,9 +22,9 @@ Engine::~Engine() {
   mks::Logger::Infof("Engine deleted");
 }
 
-Behavior Engine::GetObject(char* name) {
+std::unique_ptr<Behavior> Engine::GetObject(char* name) {
   mks::Logger::Infof("Engine.GetObject()");
-  Behavior b{};
+  auto b = std::make_unique<Behavior>();
   return b;
 }
 
--- src/lib/Vulkan.cpp
+++ src/lib/Vulkan.cpp
@@ -1,5 +1,12 @@
 #include "Vulkan.hpp"
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+#include "Logger.hpp"
+
 namespace mks {
 
 const char* ENGINE_NAME = "MKS";
